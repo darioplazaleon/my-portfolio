d8eb6221860b153f480f22cc2195c85d18d41d3b
darioplazaleon <darioplaza02@hotmail.com>
2023-11-14 12:49:38 -0300

    portfolio funcionando v0.0.7

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import vercel from "@astrojs/vercel/serverless";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  site: 'https://darioplazaleon.github.io',
-  base: '/my-portfolio',
+  // site: 'https://darioplazaleon.github.io',
+  // base: '/my-portfolio',
 });
