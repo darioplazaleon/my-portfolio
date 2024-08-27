fbac446658bd2f183e2e7233d61edbd4c2042dc0
darioplaza02 <darioplazaleon@hotmail.com>
2024-08-27 19:54:09 -0300

    infinite slider mobile working

--- src/components/TechCarousel.jsx
+++ src/components/TechCarousel.jsx
@@ -1,34 +1,19 @@
 import { useEffect } from "react";
-import { motion } from "framer-motion";
+import { animate, motion, useMotionValue } from "framer-motion";
+import useMeasure from "react-use-measure";
 
 const TechCarousel = ({ techs }) => {
-  const duplicatedSlides = [...techs, ...techs, ...techs];
 
   return (
-    <div className="relative w-full h-40 overflow-hidden  mx-auto">
-      <motion.div
-        className="flex gap-2 w-full h-full"
-        animate={{
-          x: ["0%", "-100%"],
-          transition: {
-            ease: "linear",
-            duration: 10,
-            repeat: Infinity,
-          },
-        }}
-      >
-        {duplicatedSlides.map((slide, index) => (
-          <div
-            key={index}
-            className="flex-shrink-0" style={{ width: `${100 / techs.lenght}%` }}
-          >
-            <div className="flex flex-col items-center justify-center bg-white rounded-lg w-40 h-full">
-              <img src={slide.image} alt={slide.name} className="w-20 h-full" />
-              <p>{slide.name}</p>
-            </div>
+    <div className="relative w-screen h-40 overflow-hidden  mx-auto">
+      <div className="absolute inset-0 z-20 before:absolute before:left-0 before:top-0 before:w-1/4 before:h-full before:bg-gradient-to-r before:from-ctp-crust before:to-transparent before:filter before:blur-3 after:absolute after:right-0 after:top-0 after:w-1/4 after:h-full after:bg-gradient-to-l after:from-ctp-crust after:to-transparent after:filter after:blur-3"></div>
+      <div>
+        {techs.map((tech, index) => (
+          <div key={index}>
+            <img src={tech.image} alt={tech.name} className="w-20"/>
           </div>
         ))}
-      </motion.div>
+      </div>
     </div>
   );
 };
