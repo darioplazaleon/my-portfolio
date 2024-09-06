9124bd07dfa91b2923cc52c4c73ee1f2f9805dd6
darioplaza02 <darioplaza02@hotmail.com>
2024-09-06 13:10:52 -0300

    new languagePicker i18n

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -5,17 +5,21 @@ export const languages = {
 
 export const defaultLang = "es";
 
+export const showDefaultLang = false;
+
 export const ui = {
   es: {
     "nav.home": "Inicio",
     "nav.about": "Sobre mi",
     "nav.services": "Servicios",
     "nav.contact": "Contacto",
+    "nav.projects": "Proyectos",
   },
   en: {
     "nav.home": "Home",
     "nav.about": "About",
     "nav.services": "Services",
     "nav.contact": "Contact",
+    "nav.projects": "Projects",
   },
 } as const;
