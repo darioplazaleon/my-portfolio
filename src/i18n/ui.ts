b6187afa322c9384aee8094d28a6e6f762c6b86d
darioplaza02 <darioplaza02@hotmail.com>
2024-09-06 21:54:57 -0300

    footer i18n integration

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -14,6 +14,11 @@ export const ui = {
     "nav.services": "Servicios",
     "nav.contact": "Contacto",
     "nav.projects": "Proyectos",
+    "footer.socialTitle": "Social",
+    "footer.explorerTitle": "Explora",
+    "footer.contact.phrase": "¡Di Hola!",
+    "footer.contact.email": "Contáctame",
+    "footer.copy": "© 2024 Todos los derechos reservados",
   },
   en: {
     "nav.home": "Home",
@@ -21,5 +26,10 @@ export const ui = {
     "nav.services": "Services",
     "nav.contact": "Contact",
     "nav.projects": "Projects",
+    "footer.socialTitle": "Social",
+    "footer.explorerTitle": "Explore",
+    "footer.contact.phrase": "Say Hi!",
+    "footer.contact.email": "Contact Me",
+    "footer.copy": "© 2024 All rights reserved",
   },
 } as const;
